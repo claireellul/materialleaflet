61c142a6730bc9115106819b08a459bfe44cd532
claireellul <c.ellul@ucl.ac.uk>
2017-12-31 09:37:22 -0500

    onclick on menu now returns the earthquakes on demand - note use of return false in the onclick event otherwise the map just resets due to the javascript in the template

--- uk.ac.ucl.cegeg077.ucfscde/www/js/appFunctions.js
+++ uk.ac.ucl.cegeg077.ucfscde/www/js/appFunctions.js
@@ -1,7 +1,9 @@
 // create the code to get the Earthquakes data using an XMLHttpRequest
 var client;
+var mymap;
 
 function getEarthquakes() {
+  alert("getting Earthquakes");
    client = new XMLHttpRequest();
    client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson');
    client.onreadystatechange = earthquakeResponse;  // note don't use earthquakeResponse() with brackets as that doesn't work
@@ -11,8 +13,10 @@ function getEarthquakes() {
 function earthquakeResponse() {
   // this function listens out for the server to say that the data is ready - i.e. has state 4
   if (client.readyState == 4) {
+    alert("setting earthquakes");
     // once the data is ready, process the data
     var earthquakedata = client.responseText;
+    alert(earthquakedata);
     loadGeoJSONLayer(earthquakedata);
     }
 }
